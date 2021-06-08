571a95c6eee7d8cf8abc4a72e6274c6e828e627e
aavelor <alucard03@gmail.com>
2021-06-07 21:21:04 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+Brand search autocomplete
+===
+
+This exercise is the creation of a dynamic brand search form that allows users to enter some letters of a brand name in a search box and see all the possible results below the search box.  The goal of this exercise is to generate discussion about the React component architecture, organization, and learn about your experiences and best practices when working with React.
+
+We will start with the "mockup" that is defined in `src/App.js`.  This example project is based on the default template of `create-react-app` tool.  Right now there is a single `App` component that contains basic HTML representations of all the parts of our search form, including sample results.  You can view the project by running `npm start`.
+
+Note: the "Save Selections" button is a placeholder.  We're only focused on the look and feel of this form, not submitting information anywhere.
+
+
+Preparation
+===
+
+1. Clone the Git repo to your local computer.
+2. Install necessary dependencies via `npm`.
+3. Use `npm start` to see the live representation of the mockup HTML.
+
+
+Part 1
+===
+
+The first step of this exercise is to transform the mockup HTML in the `App` component into a proper hierarchy of React components.  We will begin with a discussion of the component hierarchy and then implement each component.  The list of brands and attributes that we will use as the data source are available in `src/constants.js`.
+
+
+Part 2
+===
+
+The next step is improving the look and feel of this set of components.  We will discuss best practices for organizing the components, creating stylesheets, and ideas for ordering the results.
+
---
+++ src/App.js
@@ -0,0 +1,604 @@
+import React, { useState } from 'react';
+import './App.css'
+import brandList from "./constants";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  let brandList = [
+    {
+        "brand": "47-brand",
+        "available": 5,
+        "premium": false
+    },
+    {
+        "brand": "47 BRAND",
+        "available": 4,
+        "premium": false
+    },
+    {
+        "brand": "8IGHTH DSTRKT",
+        "available": 41,
+        "premium": false
+    },
+    {
+        "brand": "ADIDAS",
+        "available": 49,
+        "premium": true
+    },
+    {
+        "brand": "AKADEMIKS",
+        "available": 39,
+        "premium": true
+    },
+    {
+        "brand": "ALPHA INDUSTRIES",
+        "available": 1,
+        "premium": true
+    },
+    {
+        "brand": "any-memes",
+        "available": 45,
+        "premium": false
+    },
+    {
+        "brand": "BILCO INDUS",
+        "available": 40,
+        "premium": false
+    },
+    {
+        "brand": "BLACK JEANS",
+        "available": 10,
+        "premium": false
+    },
+    {
+        "brand": "BLACK KEYS",
+        "available": 31,
+        "premium": false
+    },
+    {
+        "brand": "BLACK PIKE",
+        "available": 18,
+        "premium": false
+    },
+    {
+        "brand": "Bleecker & Mercer",
+        "available": 40,
+        "premium": false
+    },
+    {
+        "brand": "BORNFLY",
+        "available": 13,
+        "premium": false
+    },
+    {
+        "brand": "CALIBER",
+        "available": 42,
+        "premium": false
+    },
+    {
+        "brand": "CAPITAL GAINS",
+        "available": 16,
+        "premium": true
+    },
+    {
+        "brand": "champion",
+        "available": 11,
+        "premium": false
+    },
+    {
+        "brand": "CHAMPION",
+        "available": 22,
+        "premium": false
+    },
+    {
+        "brand": "city-blue",
+        "available": 41,
+        "premium": true
+    },
+    {
+        "brand": "City Blue",
+        "available": 45,
+        "premium": false
+    },
+    {
+        "brand": "CITY BLUE",
+        "available": 19,
+        "premium": false
+    },
+    {
+        "brand": "converse",
+        "available": 46,
+        "premium": true
+    },
+    {
+        "brand": "CONVERSE",
+        "available": 50,
+        "premium": true
+    },
+    {
+        "brand": "COPPER RIVET",
+        "available": 28,
+        "premium": true
+    },
+    {
+        "brand": "crep-protect",
+        "available": 9,
+        "premium": true
+    },
+    {
+        "brand": "DEMOLITION",
+        "available": 4,
+        "premium": true
+    },
+    {
+        "brand": "DOPE",
+        "available": 49,
+        "premium": false
+    },
+    {
+        "brand": "DREAMLAND",
+        "available": 11,
+        "premium": false
+    },
+    {
+        "brand": "dr-martens",
+        "available": 45,
+        "premium": true
+    },
+    {
+        "brand": "DR MARTENS",
+        "available": 40,
+        "premium": false
+    },
+    {
+        "brand": "ETHIKA",
+        "available": 22,
+        "premium": false
+    },
+    {
+        "brand": "EVOLUTION",
+        "available": 31,
+        "premium": false
+    },
+    {
+        "brand": "fila",
+        "available": 17,
+        "premium": true
+    },
+    {
+        "brand": "FILA",
+        "available": 48,
+        "premium": false
+    },
+    {
+        "brand": "GALAXY",
+        "available": 14,
+        "premium": true
+    },
+    {
+        "brand": "GEORGE V",
+        "available": 44,
+        "premium": false
+    },
+    {
+        "brand": "GRAY EARTH",
+        "available": 40,
+        "premium": false
+    },
+    {
+        "brand": "INNOVATIVE SOURCING",
+        "available": 42,
+        "premium": false
+    },
+    {
+        "brand": "ITG",
+        "available": 14,
+        "premium": false
+    },
+    {
+        "brand": "jason-markk",
+        "available": 9,
+        "premium": false
+    },
+    {
+        "brand": "JASON MARKK",
+        "available": 15,
+        "premium": true
+    },
+    {
+        "brand": "jordan",
+        "available": 6,
+        "premium": false
+    },
+    {
+        "brand": "JORDAN",
+        "available": 43,
+        "premium": true
+    },
+    {
+        "brand": "JORDAN_",
+        "available": 28,
+        "premium": false
+    },
+    {
+        "brand": "JORDAN CRAIG",
+        "available": 6,
+        "premium": false
+    },
+    {
+        "brand": "JP INERNATIONAL",
+        "available": 14,
+        "premium": false
+    },
+    {
+        "brand": "KIMERA",
+        "available": 21,
+        "premium": true
+    },
+    {
+        "brand": "kixx",
+        "available": 29,
+        "premium": false
+    },
+    {
+        "brand": "K-SWISS",
+        "available": 12,
+        "premium": false
+    },
+    {
+        "brand": "lacoste",
+        "available": 21,
+        "premium": false
+    },
+    {
+        "brand": "LACOSTE",
+        "available": 43,
+        "premium": false
+    },
+    {
+        "brand": "LEGENDARY RAWYALTY",
+        "available": 44,
+        "premium": false
+    },
+    {
+        "brand": "LEGENDRY RAWYALTY",
+        "available": 21,
+        "premium": false
+    },
+    {
+        "brand": "Le TIGRE",
+        "available": 16,
+        "premium": false
+    },
+    {
+        "brand": "levis",
+        "available": 12,
+        "premium": true
+    },
+    {
+        "brand": "LEVI`S",
+        "available": 19,
+        "premium": true
+    },
+    {
+        "brand": "MAKOBI",
+        "available": 45,
+        "premium": true
+    },
+    {
+        "brand": "MAXI MILIAN",
+        "available": 17,
+        "premium": true
+    },
+    {
+        "brand": "MISCHIEF INT INC",
+        "available": 42,
+        "premium": true
+    },
+    {
+        "brand": "mitchell-ness",
+        "available": 48,
+        "premium": false
+    },
+    {
+        "brand": "MITCHELL & NESS",
+        "available": 44,
+        "premium": false
+    },
+    {
+        "brand": "M. SOCIETY",
+        "available": 34,
+        "premium": false
+    },
+    {
+        "brand": "N.E BRANDS",
+        "available": 41,
+        "premium": true
+    },
+    {
+        "brand": "NEW BALANCE",
+        "available": 30,
+        "premium": false
+    },
+    {
+        "brand": "newera",
+        "available": 9,
+        "premium": false
+    },
+    {
+        "brand": "new-era",
+        "available": 2,
+        "premium": true
+    },
+    {
+        "brand": "NEWERA",
+        "available": 48,
+        "premium": false
+    },
+    {
+        "brand": "NEW ERA",
+        "available": 46,
+        "premium": false
+    },
+    {
+        "brand": "nike",
+        "available": 35,
+        "premium": false
+    },
+    {
+        "brand": "NIKE",
+        "available": 5,
+        "premium": true
+    },
+    {
+        "brand": "NIKE_",
+        "available": 41,
+        "premium": false
+    },
+    {
+        "brand": "NORTH`S REPUBLIC",
+        "available": 29,
+        "premium": false
+    },
+    {
+        "brand": "PARTISAN",
+        "available": 43,
+        "premium": false
+    },
+    {
+        "brand": "PLANET OF THE GRAPES",
+        "available": 39,
+        "premium": false
+    },
+    {
+        "brand": "pony",
+        "available": 43,
+        "premium": true
+    },
+    {
+        "brand": "PONY",
+        "available": 36,
+        "premium": false
+    },
+    {
+        "brand": "pro-standard",
+        "available": 32,
+        "premium": false
+    },
+    {
+        "brand": "PRO STANDARD",
+        "available": 5,
+        "premium": false
+    },
+    {
+        "brand": "puma",
+        "available": 18,
+        "premium": false
+    },
+    {
+        "brand": "PUMA",
+        "available": 48,
+        "premium": false
+    },
+    {
+        "brand": "PUMA_",
+        "available": 26,
+        "premium": false
+    },
+    {
+        "brand": "RAP VINTAGE",
+        "available": 14,
+        "premium": true
+    },
+    {
+        "brand": "REASON",
+        "available": 15,
+        "premium": false
+    },
+    {
+        "brand": "REBEL MINDS",
+        "available": 6,
+        "premium": false
+    },
+    {
+        "brand": "reebok",
+        "available": 50,
+        "premium": true
+    },
+    {
+        "brand": "REEBOK",
+        "available": 22,
+        "premium": false
+    },
+    {
+        "brand": "REGAL WEAR",
+        "available": 28,
+        "premium": true
+    },
+    {
+        "brand": "RIFLESSI",
+        "available": 12,
+        "premium": false
+    },
+    {
+        "brand": "RIVELLI",
+        "available": 15,
+        "premium": false
+    },
+    {
+        "brand": "ROBERT PHILLIPE",
+        "available": 40,
+        "premium": true
+    },
+    {
+        "brand": "ROCAWEAR",
+        "available": 29,
+        "premium": false
+    },
+    {
+        "brand": "ROYAL BLUE",
+        "available": 31,
+        "premium": false
+    },
+    {
+        "brand": "rwtw",
+        "available": 33,
+        "premium": true
+    },
+    {
+        "brand": "RWTW",
+        "available": 17,
+        "premium": false
+    },
+    {
+        "brand": "saucony",
+        "available": 16,
+        "premium": false
+    },
+    {
+        "brand": "SAUCONY",
+        "available": 44,
+        "premium": false
+    },
+    {
+        "brand": "SCHOTT",
+        "available": 4,
+        "premium": true
+    },
+    {
+        "brand": "SMOKE RISE",
+        "available": 2,
+        "premium": false
+    },
+    {
+        "brand": "snkr-project",
+        "available": 4,
+        "premium": true
+    },
+    {
+        "brand": "SOF SOLE",
+        "available": 23,
+        "premium": true
+    },
+    {
+        "brand": "sprayground",
+        "available": 19,
+        "premium": false
+    },
+    {
+        "brand": "STALL&DEAN",
+        "available": 48,
+        "premium": true
+    },
+    {
+        "brand": "timberland",
+        "available": 39,
+        "premium": false
+    },
+    {
+        "brand": "TIMBERLAND",
+        "available": 41,
+        "premium": true
+    },
+    {
+        "brand": "top-culture",
+        "available": 9,
+        "premium": false
+    },
+    {
+        "brand": "TOP CULTURE",
+        "available": 39,
+        "premium": true
+    },
+    {
+        "brand": "UNDER ARMOUR",
+        "available": 33,
+        "premium": false
+    },
+    {
+        "brand": "VALEZA",
+        "available": 3,
+        "premium": false
+    },
+    {
+        "brand": "WAIMEA",
+        "available": 12,
+        "premium": false
+    },
+    {
+        "brand": "X-RAY JEANS",
+        "available": 21,
+        "premium": false
+    },
+    {
+        "brand": "",
+        "available": 32,
+        "premium": false
+    }
+];
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setInput(e.target.value);
+  };
+
+  if(input.length > 0){
+    brandList = brandList.filter((i) => {
+      return i.brand.match(input);
+    })
+  }
+
+  return (
+    <div className="App">
+      <p className="App-Title">Search for a brand:</p>
+      <p className="App-Subtitle">Brand name:
+      
+      <input type="text"
+        placeholder="Search here"
+        onChange={handleChange}
+        value={input}
+      />
+      </p>
+
+    <div className="results">
+      {brandList.map((brand, index) => {
+        return(
+          <div className={input.length > 0 ? "App-Result" : "hidden"} key={index}>
+            <ul>
+              <li className="App-brandName">{brand.brand}</li>
+              <span className="App-Stock"> Stock: {brand.available}</span>
+              {brand.premium ? <span className="App-Premium">premium</span> : ''}
+              
+            </ul>
+          </div>
+        )
+      })}
+      
+      <button className="App-Button">Save selections</button>
+
+    </div>
+    </div>
+  );
+}
+
+export default App;
